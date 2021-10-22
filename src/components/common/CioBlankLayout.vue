84c40eb7ffc1102147fbf131912e51a7939c056d
catalin.cimpoeru <catalin.cimpoeru@orange.com>
2021-10-22 18:57:26 +0300

    add basic layout

---
+++ src/components/common/CioBlankLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title> {{ title }} </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <slot />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonHeader,
+  IonTitle,
+  IonContent,
+  IonToolbar,
+  IonPage,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    IonContent,
+    IonTitle,
+    IonHeader,
+    IonToolbar,
+    IonPage,
+  },
+
+  props: {
+    title: String,
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,27 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+  <cio-blank-layout title="My Home Title">
+    <slot>
+
+    </slot>
+  </cio-blank-layout>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+
+import CioBlankLayout from "../components/common/CioBlankLayout.vue";
 
 export default defineComponent({
-  name: 'Home',
   components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  }
+    CioBlankLayout,
+  },
 });
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,9 +37,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
